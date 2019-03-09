2325820a8b2dae3c3ea6bd1558af5877cc12961a
Stanislav <s_admin@mail.ru>
2019-03-09 22:41:07 +0300

    fix

--- database/migrations/2019_03_04_122643_create_domains_table.php
+++ database/migrations/2019_03_04_122643_create_domains_table.php
@@ -15,7 +15,7 @@ class CreateDomainsTable extends Migration
     {
         Schema::create('domains', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name', 100)->unique();
+            $table->string('name', 255);
             $table->timestamp('created_at')->useCurrent();
             $table->timestamp('updated_at');
             
